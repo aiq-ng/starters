f5c39c10c39c821812e4b00795b354fc3cc090bd
Hordunlarmy <Hordunlarmy@gmail.com>
2024-10-21 10:51:51 +0100

    feat: Implement CORS support for API

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+// Handle CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// If it's an OPTIONS request, return a response immediately
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/loadenv.php';
