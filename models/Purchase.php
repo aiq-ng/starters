bf8a3b6ff98b71c5c3cb9b99338ee7c2a11c83e9
Hordunlarmy <Hordunlarmy@gmail.com>
2025-02-07 11:20:08 +0100

    feat(Purchase): Add logging to purchase creation

--- models/Purchase.php
+++ models/Purchase.php
@@ -219,6 +219,9 @@ class Purchase
 
     public function createPurchase($data)
     {
+        error_log('Creating purchase order...');
+        error_log(json_encode($data));
+
         $this->db->beginTransaction();
 
         try {
