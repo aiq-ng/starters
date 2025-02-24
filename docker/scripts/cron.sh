9bbb576eaf14fb134f37e60240d5f25cde4f251e
Hordunlarmy <Hordunlarmy@gmail.com>
2025-02-24 18:18:07 +0100

    fix(docker): Correct PHP binary path in cron script

--- docker/scripts/cron.sh
+++ docker/scripts/cron.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+PHP_BINARY="/usr/local/bin/php"
 LOG_FILE="/workspace/starters/app/listener.log"
 ENV_FILE="/workspace/starters/app/.env"
+SCRIPT_PATH="/workspace/starters/app/listener.php"
 MAX_LINES=1000
 
-init_log_file() {
+init() {
 	touch "$LOG_FILE"
 	chmod 664 "$LOG_FILE"
+	chmod +x "$SCRIPT_PATH"
 }
 
 # Function to trim the log file if it exceeds MAX_LINES
@@ -32,7 +35,7 @@ load_env() {
 
 run_script() {
 	echo "[$(date '+%Y-%m-%d %H:%M:%S')] Running listener.php" >>"$LOG_FILE"
-	php listener.php >>"$LOG_FILE" 2>&1
+	"$PHP_BINARY" "$SCRIPT_PATH" >>"$LOG_FILE" 2>&1
 	local exit_code=$?
 
 	if [ "$exit_code" -eq 0 ]; then
@@ -43,7 +46,7 @@ run_script() {
 }
 
 main() {
-	init_log_file
+	init
 	load_env
 	trim_log_file
 	run_script
